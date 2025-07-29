e8b5c6629ce5deef17f514c53f11d7ef6a4085ec
ahrisam <ahrisam3@outlook.com>
2025-07-29 23:36:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 @app.route("/", methods=["GET", "POST"])
-def start():
+def home():
     if request.method == "POST":
         return redirect(url_for("login"))
     return render_template("landing.html")
@@ -12,13 +13,25 @@ def start():
 def login():
     if request.method == "POST":
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
