d2a2f20f3066f82f25e252700413a27675d501f6
ahrisam <ahrisam3@outlook.com>
2025-07-29 21:42:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def start():
     if request.method == "POST":
-        return "<h2>hello</h2>"
+        return redirect(url_for("login"))
     return render_template("landing.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # if request.method == "POST":
+    #     user = request.form[]
+    #     return redirect
+    # else:
+        return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
