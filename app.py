e97fdafb718f3805a4cc49779d7d7b53666f9dbe
ahrisam <ahrisam3@outlook.com>
2025-07-30 03:01:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, request, render_template, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
 app.secret_key = "hello"
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
 
-
 db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    email = db.Column("name", db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
@@ -38,4 +49,5 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
