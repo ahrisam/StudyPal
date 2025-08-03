17aa470f2444b316a44fb47262ac47add236bf77
ahrisam <ahrisam3@outlook.com>
2025-08-03 16:51:12 +0000

    Update to app and profile

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, request, render_template, url_for, redirect, session, f
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from itsdangerous import URLSafeSerializer
+# from itsdangerous import URLSafeSerializer
 from dotenv import load_dotenv
 from flask_dance.contrib.google import make_google_blueprint, google
 import os
 
+#Initialising the Flask app
 app = Flask(__name__)
+
+#Configurations
 load_dotenv()
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
@@ -16,12 +19,13 @@ app.config["GOOGLE_CLIENT_SECRET"] = os.getenv("GOOGLE_CLIENT_SECRET")
 app.config["Google_CLIENT_ID"] = os.getenv("Google_CLIENT_ID")
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+#Initialising extentions
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-
-
 db = SQLAlchemy(app)
+
+#Setting Up google OAuth
 google_bp = make_google_blueprint(
     client_id = os.getenv("Google_CLIENT_ID"),
     client_secret = os.getenv("GOOGLE_CLIENT_SECRET"),
@@ -30,35 +34,45 @@ google_bp = make_google_blueprint(
 )
 app.register_blueprint(google_bp, url_prefix = "/login")
 
+#Setting up the Database; using SQLAlchemy and Flask-Login intergration
 class User(db.Model, UserMixin):
     __tablename__ = "users"
-    id = db.Column("id", db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    password = db.Column(db.String(200))
+    email = db.Column(db.String(100), unique = True)
+    password = db.Column(db.String(200), nullable = False)
 
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
 
+#DB for user profile
 class Profile(db.Model):
-    _id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     fname = db.Column(db.String(100))
-    avatar = db.Column(db.String(100))
+    avatar = db.Column(db.String(255))
     age = db.Column(db.String(40))
 
 
-    def __init__(self,fname,age):
+    def __init__(self, fname, age, avatar):
         self.fname = fname
         self.age = age
+        self.avatar = avatar
 
+#Welcome page/landing page
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         return redirect(url_for("login"))
     return render_template("landing.html")
 
+#Load user function required by flask
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#Login
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -74,10 +88,7 @@ def login():
     return render_template("login.html")
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
+#Google login
 @app.route("/login/google/authorized")
 def google_login():
     if not google.authorized:
@@ -121,10 +132,12 @@ def profile_set():
     if request.method == "POST":
         fullname = request.form["fullname"]
         age = request.form["age"]
-        # avatar = request.form["avatar"]
+        avatar = request.form["avatar"]
+
         existing_profile = Profile.query.filter_by(fname = fullname).first()
         if existing_profile:
             return redirect(url_for("dashboard"))
+        
         new_profile = Profile(fullname, age)
         db.session.add(new_profile)
         db.session.commit()
@@ -132,8 +145,9 @@ def profile_set():
     return render_template("profileset.html")
 
 @app.route("/logout")
+@login_required
 def logout():
-    session.pop("user", None)
+    login_user()
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
